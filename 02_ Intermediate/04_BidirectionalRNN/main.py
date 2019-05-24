698aa4498915b3632c1cb2bffa16a86254d026e0
ethon_tang <ethon_tang@foxmail.com>
2019-05-24 16:09:07 +0800

    RNN(mini lstm)/BiRNN

--- 02_ Intermediate/04_BidirectionalRNN/main.py
+++ 02_ Intermediate/04_BidirectionalRNN/main.py
@@ -33,23 +33,23 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size,
                                           shuffle=False)
 
-class RNN(nn.Module):
+class BiRNN(nn.Module):
     def __init__(self,input_size,hidden_size,num_layers,num_classes):
-        super(RNN, self).__init__()
+        super(BiRNN, self).__init__()
         self.hidden_size=hidden_size
         self.num_layers=num_layers
-        self.lstm=nn.LSTM(input_size,hidden_size,num_layers,batch_first=True)
-        self.fc=nn.Linear(hidden_size,num_classes)
+        self.lstm=nn.LSTM(input_size,hidden_size,num_layers,batch_first=True,bidirectional=True)
+        self.fc=nn.Linear(hidden_size*2,num_classes)# BIRNN size stack 2 layer
 
     def forward(self, x):
-        h0=torch.zeros(self.num_layers,x.size(0),self.hidden_size).to(device)
-        c0=torch.zeros(self.num_layers,x.size(0),self.hidden_size).to(device)
+        h0=torch.zeros(self.num_layers*2,x.size(0),self.hidden_size).to(device)
+        c0=torch.zeros(self.num_layers*2,x.size(0),self.hidden_size).to(device)
 
         out,_=self.lstm(x,(h0,c0))
         out=self.fc(out[:,-1,:])
         return out
 
-model=RNN(input_size,hidden_size,num_layers,num_classes).to(device)
+model=BiRNN(input_size,hidden_size,num_layers,num_classes).to(device)
 
 criterion=nn.CrossEntropyLoss()
 optimizer=torch.optim.Adam(model.parameters(),lr=lr)
